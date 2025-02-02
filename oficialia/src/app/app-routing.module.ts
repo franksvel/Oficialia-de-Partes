a221ca36699413705c0c70c9cc0e984a03d564e0
franksvel <frankedy.sanchez.velasco@gmail.com>
2025-02-01 21:58:03 -0600

    inicio de proyecto

---
+++ oficialia/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const routes: Routes = [
+{ path: '', redirectTo: '/login', pathMatch: 'full' }, 
+{ path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+
+
+
+{path: 'login', component: LoginComponent},
+{path: 'dashboard', component: DashboardComponent},
+];
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { 
+
+
+}
---
+++ oficialia/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    LoginComponent,
+    NavComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
