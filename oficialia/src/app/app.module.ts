6ab781e00447bf2f54cae39334e96846497be4e1
franksvel <frankedy.sanchez.velasco@gmail.com>
2025-02-10 22:24:03 -0600

    instalacion de ng add @angular/fire

--- oficialia/src/app/app.module.ts
+++ oficialia/src/app/app.module.ts
@@ -12,7 +12,12 @@ import { DashcontainerComponent } from './components/dashcontainer/dashcontainer
 
 import { OficeComponent } from './pages/ofice/ofice.component';
 import { CrudtableComponent } from './components/crudtable/crudtable.component';
-import {OAuthModule } from 'angular-oauth2-oidc'
+import {OAuthModule } from 'angular-oauth2-oidc';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { SignUpComponent } from './auth/features/sign-up/sign-up.component';
+import { SignInComponent } from './auth/features/sign-in/sign-in.component'
 
 
 
@@ -29,7 +34,9 @@ import {OAuthModule } from 'angular-oauth2-oidc'
     CardComponent,
     DashcontainerComponent,
     OficeComponent,
-    CrudtableComponent
+    CrudtableComponent,
+    SignUpComponent,
+    SignInComponent
 
   ],
   imports: [
@@ -45,6 +52,9 @@ import {OAuthModule } from 'angular-oauth2-oidc'
   ],
   providers: [
     provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp({ projectId: "oficialia-d3a32", appId: "1:849796925257:web:d6279ea69ecb45aee8bc98", storageBucket: "oficialia-d3a32.firebasestorage.app", apiKey: "AIzaSyBBNd7sZ6P-uyQOIVL0qgdtbv4Am7oEkzg", authDomain: "oficialia-d3a32.firebaseapp.com", messagingSenderId: "849796925257", measurementId: "G-6YSJ0BKR92" })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
 
   ],
   bootstrap: [AppComponent]
